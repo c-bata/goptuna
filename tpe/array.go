4122a4fbd957bc1a84b380335e8f5896fc0d0806
c-bata <contact@c-bata.link>
2019-07-24 16:02:53 +0900

    Implement numpy argsort

--- tpe/array.go
+++ tpe/array.go
@@ -1,5 +1,7 @@
 package tpe
 
+import "sort"
+
 func ones(size int) []float64 {
 	ones := make([]float64, size)
 	for i := 0; i < size; i++ {
@@ -26,4 +28,31 @@ func linspace(start, stop float64, num int, endPoint bool) []float64 {
 		r[i] = start + float64(i)*step
 	}
 	return r
+}
+
+func ArgSort2DFloat64(lossVals [][2]float64) []int {
+	type sortable struct {
+		index   int
+		lossVal [2]float64
+	}
+	x := make([]sortable, len(lossVals))
+	for i := 0; i < len(lossVals); i++ {
+		x[i] = sortable{
+			index:   i,
+			lossVal: lossVals[i],
+		}
+	}
+
+	sort.SliceStable(x, func(i, j int) bool {
+		if x[i].lossVal[0] == x[j].lossVal[0] {
+			return x[i].lossVal[1] < x[j].lossVal[1]
+		}
+		return x[i].lossVal[0] < x[j].lossVal[0]
+	})
+
+	results := make([]int, len(x))
+	for i := 0; i < len(x); i++ {
+		results[i] = x[i].index
+	}
+	return results
 }
--- tpe/array_test.go
+++ tpe/array_test.go
@@ -1,6 +1,7 @@
 package tpe
 
 import (
+	"math"
 	"reflect"
 	"testing"
 )
@@ -38,6 +39,51 @@ func Test_linspace(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := linspace(tt.args.start, tt.args.stop, tt.args.num, tt.args.endPoint); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("linspace() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestArgSort2DFloat64(t *testing.T) {
+	type args struct {
+		lossVals [][2]float64
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{
+			name: "test case 1",
+			args: args{
+				lossVals: [][2]float64{
+					{math.Inf(-1), 93.80856756}, {math.Inf(-1), 85.64538195},
+					{math.Inf(-1), 44.58783514}, {math.Inf(-1), 4.23458368},
+					{math.Inf(-1), 42.17125041}, {math.Inf(-1), 62.14283937},
+					{math.Inf(-1), 94.45778947}, {math.Inf(-1), 64.66469149},
+					{math.Inf(-1), 36.1033201}, {math.Inf(-1), 105.69868952},
+				},
+			},
+			want: []int{3, 8, 4, 2, 5, 7, 1, 0, 6, 9},
+		},
+		{
+			name: "test case 2",
+			args: args{
+				lossVals: [][2]float64{
+					{3.0, 93.80856756}, {5.0, 85.64538195},
+					{math.Inf(-1), 44.58783514}, {math.Inf(-1), 4.23458368},
+					{math.Inf(-1), 42.17125041}, {math.Inf(-1), 62.14283937},
+					{math.Inf(-1), 94.45778947}, {math.Inf(-1), 64.66469149},
+					{math.Inf(-1), 36.1033201}, {math.Inf(-1), 105.69868952},
+				},
+			},
+			want: []int{3, 8, 4, 2, 5, 7, 6, 9, 0, 1},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := ArgSort2DFloat64(tt.args.lossVals); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("ArgSort2DFloat64() = %v, want %v", got, tt.want)
 			}
 		})
 	}
