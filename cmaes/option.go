df27d4fa9259933db9c9cda5c4b49439afe63764
c-bata <contact@c-bata.link>
2020-01-29 12:41:20 +0900

    Add a base implementation of CMA-ES sampler

---
+++ cmaes/option.go
@@ -0,0 +1,15 @@
+package cmaes
+
+import (
+	"math/rand"
+)
+
+// SamplerOption is a type of the function to customizing CMA-ES sampler.
+type SamplerOption func(sampler *Sampler)
+
+// SamplerOptionSeed sets seed number.
+func SamplerOptionSeed(seed int64) SamplerOption {
+	return func(sampler *Sampler) {
+		sampler.rng = rand.New(rand.NewSource(seed))
+	}
+}
---
+++ cmaes/sampler.go
@@ -0,0 +1,61 @@
+package cmaes
+
+import (
+	"math/rand"
+
+	"github.com/c-bata/goptuna"
+)
+
+var _ goptuna.RelativeSampler = &Sampler{}
+
+// Sampler returns the next search points by using CMA-ES.
+type Sampler struct {
+	rng            *rand.Rand
+	nStartUpTrials int
+}
+
+func (s *Sampler) SampleRelative(
+	study *goptuna.Study,
+	trial goptuna.FrozenTrial,
+	searchSpace map[string]interface{},
+) (map[string]float64, error) {
+	if searchSpace == nil || len(searchSpace) == 0 {
+		return nil, nil
+	}
+
+	if len(searchSpace) == 1 {
+		// CMA-ES does not support optimization of 1-D search space.
+		return nil, goptuna.ErrUnsupportedSearchSpace
+	}
+
+	trials, err := study.GetTrials()
+	if err != nil {
+		return nil, err
+	}
+	completed := make([]goptuna.FrozenTrial, 0, len(trials))
+	for i := range trials {
+		if trials[i].State == goptuna.TrialStateComplete {
+			completed = append(completed, trials[i])
+		}
+	}
+
+	if len(completed) < s.nStartUpTrials {
+		return nil, err
+	}
+
+	params := make(map[string]float64, len(searchSpace))
+	return params, nil
+}
+
+// NewSampler returns the TPE sampler.
+func NewSampler(opts ...SamplerOption) *Sampler {
+	sampler := &Sampler{
+		rng:            rand.New(rand.NewSource(0)),
+		nStartUpTrials: 0,
+	}
+
+	for _, opt := range opts {
+		opt(sampler)
+	}
+	return sampler
+}
