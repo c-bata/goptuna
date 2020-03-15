1f3503b534fe16c68053338a8e68b26b65eafec9
c-bata <contact@c-bata.link>
2020-03-16 01:24:39 +0900

    Update benchmark scritps

--- _benchmarks/run_sigopt_ackley.sh
+++ _benchmarks/run_sigopt_ackley.sh
@@ -9,11 +9,10 @@ go build -o ./cma_solver ./_benchmarks/cma_solver/main.go
 
 RANDOM_SOLVER=$($KUROBAKO solver random)
 TPE_SOLVER=$($KUROBAKO solver command ./tpe_solver)
-CMA_SOLVER=$($KUROBAKO solver command ./cma_solver)
 PROBLEM=$($KUROBAKO problem sigopt --dim 5 ackley)
 
 $KUROBAKO studies \
-  --solvers $RANDOM_SOLVER $TPE_SOLVER $CMA_SOLVER \
+  --solvers $RANDOM_SOLVER $TPE_SOLVER \
   --problems $PROBLEM \
   --repeats 100 --budget 100 \
   | $KUROBAKO run --parallelism 4 > $1
