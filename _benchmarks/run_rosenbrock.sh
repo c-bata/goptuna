dd470bde0e024a574ca6bf0b03785be4435fd196
c-bata <contact@c-bata.link>
2020-03-19 00:02:25 +0900

    Remove CMA solver for kurobako because relative sampler is unsupported

--- _benchmarks/run_rosenbrock.sh
+++ _benchmarks/run_rosenbrock.sh
@@ -4,18 +4,21 @@ set -e
 
 KUROBAKO=${KUROBAKO:-kurobako}
 
+# go build -o ./cma_solver ./_benchmarks/cma_solver/main.go
 go build -o ./tpe_solver ./_benchmarks/tpe_solver/main.go
-go build -o ./cma_solver ./_benchmarks/cma_solver/main.go
 go build -o ./rosenbrock_problem ./_benchmarks/rosenbrock_problem/main.go
 
 RANDOM_SOLVER=$($KUROBAKO solver random)
-CMA_SOLVER=$($KUROBAKO solver command ./cma_solver)
+
+# kurobako-go don't still support relative sampler.
+# CMA_SOLVER=$($KUROBAKO solver command ./cma_solver)
+
 TPE_SOLVER=$($KUROBAKO solver command ./tpe_solver)
 OPTUNA_SOLVER=$($KUROBAKO solver command python ./_benchmarks/optuna_solver/cmaes.py)
 PROBLEM=$($KUROBAKO problem command ./rosenbrock_problem)
 
 $KUROBAKO studies \
-  --solvers $RANDOM_SOLVER $TPE_SOLVER $CMA_SOLVER $OPTUNA_SOLVER \
+  --solvers $RANDOM_SOLVER $TPE_SOLVER $OPTUNA_SOLVER \
   --problems $PROBLEM \
   --repeats 8 --budget 300 \
   | $KUROBAKO run --parallelism 1 > $1
