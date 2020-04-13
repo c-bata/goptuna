ca995393f344a24263d453f860afcf4fe0e3dff9
c-bata <contact@c-bata.link>
2020-04-13 12:20:00 +0900

    Update examples

--- _examples/cmaes/blackhole/main.go
+++ _examples/cmaes/blackhole/main.go
@@ -16,21 +16,38 @@ func objective(trial goptuna.Trial) (float64, error) {
 
 func main() {
 	relativeSampler := cmaes.NewSampler(
-		cmaes.SamplerOptionNStartupTrials(5))
+		cmaes.SamplerOptionNStartupTrials(0))
 	study, err := goptuna.CreateStudy(
 		"goptuna-example",
+		goptuna.StudyOptionStorage(goptuna.NewBlackholeStorage(20)),
 		goptuna.StudyOptionRelativeSampler(relativeSampler),
+		goptuna.StudyOptionDefineSearchSpace(map[string]interface{}{
+			"x1": goptuna.UniformDistribution{
+				High: 10,
+				Low:  -10,
+			},
+			"x2": goptuna.UniformDistribution{
+				High: 10,
+				Low:  -10,
+			},
+		}),
 	)
 	if err != nil {
 		log.Fatal("failed to create study:", err)
 	}
 
-	if err = study.Optimize(objective, 200); err != nil {
+	if err = study.Optimize(objective, 10000); err != nil {
 		log.Fatal("failed to optimize:", err)
 	}
 
-	v, _ := study.GetBestValue()
-	params, _ := study.GetBestParams()
+	v, err := study.GetBestValue()
+	if err != nil {
+		log.Fatal("failed to get best value:", err)
+	}
+	params, err := study.GetBestParams()
+	if err != nil {
+		log.Fatal("failed to get best params:", err)
+	}
 	log.Printf("Best evaluation=%f (x1=%f, x2=%f)",
 		v, params["x1"].(float64), params["x2"].(float64))
 }
