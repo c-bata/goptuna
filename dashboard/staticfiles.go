8ce5539cb6dad8d98699134d2d83180c57f0b0dd
c-bata <contact@c-bata.link>
2020-10-21 21:43:20 +0900

    Embed productioin js file

--- dashboard/staticfiles.go
+++ dashboard/staticfiles.go
@@ -2,8 +2,56 @@
 
 package dashboard
 
-import "github.com/gorilla/mux"
+import (
+	"io/ioutil"
+	"net/http"
+	"path"
+	"strings"
+
+	"github.com/gorilla/mux"
+	"github.com/rakyll/statik/fs"
+
+	_ "github.com/c-bata/goptuna/dashboard/statik"
+)
 
 func registerStaticFileRoutes(r *mux.Router, prefix string) error {
-	panic("not implemented") // TODO(c-bata): embed static files in Go
+	statikFS, err := fs.New()
+	if err != nil {
+		return err
+	}
+	files := []string{
+		"/bundle.js",
+		"/bundle.js.map",
+		"/bundle.js.LICENSE.txt",
+	}
+	for _, filepath := range files {
+		file, err := statikFS.Open(filepath)
+		if err != nil {
+			return err
+		}
+		defer file.Close()
+		data, err := ioutil.ReadAll(file)
+		if err != nil {
+			return err
+		}
+		var contentType string
+		if strings.HasSuffix(filepath, ".css") {
+			contentType = "text/css"
+		} else if strings.HasSuffix(filepath, ".js") {
+			contentType = "application/javascript"
+		} else {
+			contentType = "text/plain"
+		}
+		handler := func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Set("Content-Type", contentType)
+			w.Write(data)
+		}
+		urlpath := filepath[len("/"):]
+		urlpath = path.Join(prefix, urlpath)
+		if urlpath[0] != '/' {
+			urlpath = "/" + urlpath
+		}
+		r.Path(urlpath).Methods("Get").HandlerFunc(handler)
+	}
+	return nil
 }
--- go.mod
+++ go.mod
@@ -5,10 +5,11 @@ go 1.13
 require (
 	github.com/go-gota/gota v0.10.1
 	github.com/google/uuid v1.1.1
+	github.com/gorilla/mux v1.8.0
 	github.com/jinzhu/gorm v1.9.10
+	github.com/rakyll/statik v0.1.7
 	github.com/sile/kurobako-go v0.0.0-20200319013303-95985b35db7b
 	github.com/spf13/cobra v0.0.5
-	github.com/gorilla/mux v1.8.0
 	gonum.org/v1/gonum v0.7.0
 	gorgonia.org/gorgonia v0.9.10
 	gorgonia.org/tensor v0.9.6
