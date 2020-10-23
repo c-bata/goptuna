ad2b697d52085fc07610d6cb54df0b1f85aa9aa2
c-bata <contact@c-bata.link>
2020-10-23 22:57:47 +0900

    Add study collapse body for attrs

--- dashboard/src/components/studyList.tsx
+++ dashboard/src/components/studyList.tsx
@@ -1,7 +1,15 @@
 import React, { FC } from "react"
 import { Link } from "react-router-dom"
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles"
-import { AppBar, Toolbar, Typography, Container, Card } from "@material-ui/core"
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Container,
+  Card,
+  Grid,
+  Box,
+} from "@material-ui/core"
 
 import { actionCreator } from "../action"
 import { formatDate } from "../dateUtil"
@@ -41,12 +49,62 @@ export const StudyList: FC<{}> = () => {
       ),
     },
     {
+      field: "direction",
+      label: "Direction",
+      sortable: false,
+      toCellValue: (i) => studies[i].direction.toString(),
+    },
+    {
+      field: "best_trial",
+      label: "Best value",
+      sortable: false,
+      toCellValue: (i) => studies[i].best_trial?.value || null,
+    },
+    {
       field: "datetime_start",
       label: "Datetime start",
       sortable: false,
       toCellValue: (i) => formatDate(studies[i].datetime_start),
     },
   ]
+
+  const collapseAttrColumns: DataGridColumn<Attribute>[] = [
+    { field: "key", label: "Key", sortable: true },
+    { field: "value", label: "Value", sortable: true },
+  ]
+
+  const collapseBody = (index: number) => {
+    return (
+      <Grid container direction="row">
+        <Grid item xs={6}>
+          <Box margin={1}>
+            <Typography variant="h6" gutterBottom component="div">
+              Study user attributes
+            </Typography>
+            <DataGrid<Attribute>
+              columns={collapseAttrColumns}
+              rows={studies[index].user_attrs}
+              keyField={"key"}
+              dense={true}
+            />
+          </Box>
+        </Grid>
+        <Grid item xs={6}>
+          <Box margin={1}>
+            <Typography variant="h6" gutterBottom component="div">
+              Study system attributes
+            </Typography>
+            <DataGrid<Attribute>
+              columns={collapseAttrColumns}
+              rows={studies[index].system_attrs}
+              keyField={"key"}
+              dense={true}
+            />
+          </Box>
+        </Grid>
+      </Grid>
+    )
+  }
 
   return (
     <div>
@@ -63,6 +121,7 @@ export const StudyList: FC<{}> = () => {
             columns={columns}
             rows={studies}
             keyField={"study_id"}
+            collapseBody={collapseBody}
           />
         </Card>
       </Container>
