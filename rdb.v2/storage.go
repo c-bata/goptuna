9a220549ebc472cdf04385bad703cf1970a2ec82
c-bata <contact@c-bata.link>
2020-10-28 22:24:45 +0900

    Locking study while cloning a trial

--- rdb.v2/storage.go
+++ rdb.v2/storage.go
@@ -284,6 +284,24 @@ func (s *Storage) CreateNewTrial(studyID int) (int, error) {
 func (s *Storage) CloneTrial(studyID int, baseTrial goptuna.FrozenTrial) (int, error) {
 	var trialID int
 	err := s.db.Transaction(func(tx *gorm.DB) error {
+		var study studyModel
+		var result *gorm.DB
+		if s.config.Dialector.Name() == "sqlite3" {
+			// TODO(c-bata): Fix concurrency problem on SQLite3.
+			// SQLite3 cannot interpret `FOR UPDATE` clause.
+			result = tx.First(&study, "study_id = ?", studyID)
+		} else {
+			// Locking within a study is necessary since the creation of a trial is not an
+			// atomic operation. More precisely, the trial number computed in
+			result = tx.Set("gorm:query_option", "FOR UPDATE").
+				First(&study, "study_id = ?", studyID)
+		}
+		// TODO(c-bata): Catch deadlock error and retry.
+		// https://dev.mysql.com/doc/refman/5.7/en/innodb-deadlocks-handling.html
+		if result.Error != nil {
+			return result.Error
+		}
+
 		// Because only `RUNNING` trials can be updated,
 		// we temporarily set the state of the new trial to `RUNNING`.
 		// After all fields of the trial have been updated,
