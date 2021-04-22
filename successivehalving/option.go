56bbce2dfdcca9b51617dd41c7097764a491465a
c-bata <contact@c-bata.link>
2021-04-23 01:42:49 +0900

    Rename successive halving option

--- successivehalving/option.go
+++ successivehalving/option.go
@@ -3,26 +3,38 @@ package successivehalving
 // Option to pass the custom option
 type Option func(pruner *Pruner) error
 
-// OptionSetMinResource to set the minimum resource.
-func OptionSetMinResource(minResource int) Option {
+// OptionMinResource to set the minimum resource.
+func OptionMinResource(minResource int) Option {
 	return func(p *Pruner) error {
 		p.MinResource = minResource
 		return nil
 	}
 }
 
-// OptionSetReductionFactor to set the reduction factor.
-func OptionSetReductionFactor(reductionFactor int) Option {
+// OptionReductionFactor to set the reduction factor.
+func OptionReductionFactor(reductionFactor int) Option {
 	return func(p *Pruner) error {
 		p.ReductionFactor = reductionFactor
 		return nil
 	}
 }
 
-// OptionSetMinEarlyStoppingRate to set the minimum value of the early stopping rate.
-func OptionSetMinEarlyStoppingRate(minEarlyStoppingRate int) Option {
+// OptionMinEarlyStoppingRate to set the minimum value of the early stopping rate.
+func OptionMinEarlyStoppingRate(minEarlyStoppingRate int) Option {
 	return func(p *Pruner) error {
 		p.MinEarlyStoppingRate = minEarlyStoppingRate
 		return nil
 	}
 }
+
+// OptionSetMinResource to set the minimum resource.
+// Deprecated: please use OptionMinResource instead.
+var OptionSetMinResource = OptionMinResource
+
+// OptionSetReductionFactor to set the reduction factor.
+// Deprecated: please use OptionReductionFactor instead.
+var OptionSetReductionFactor = OptionReductionFactor
+
+// OptionSetMinEarlyStoppingRate to set the minimum value of the early stopping rate.
+// Deprecated: please use OptionMinEarlyStoppingRate instead.
+var OptionSetMinEarlyStoppingRate = OptionMinEarlyStoppingRate
