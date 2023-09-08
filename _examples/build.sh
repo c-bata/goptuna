d5e42e18b22948236f45aacbfb436681eb691990
c-bata <contact@c-bata.link>
2023-09-08 23:42:12 +0900

    Fix go-examples action

--- _examples/build.sh
+++ _examples/build.sh
@@ -10,11 +10,11 @@ set -ex
 
 go build -o ${BIN_DIR}/cmaes ${DIR}/cmaes/main.go
 go build -o ${BIN_DIR}/cmaes_blackhole ${DIR}/cmaes/blackhole/main.go
-go build -o ${BIN_DIR}/concurrency ${DIR}/concurrency/main.go
 go build -o ${BIN_DIR}/enqueue_trial ${DIR}/enqueue_trial/main.go
 go build -o ${BIN_DIR}/trialnotify ${DIR}/trialnotify/main.go
 go build -o ${BIN_DIR}/signalhandling ${DIR}/signalhandling/main.go
 go build -o ${BIN_DIR}/simple_rdb ${DIR}/simple_rdb/main.go
 go build -o ${BIN_DIR}/simple_tpe ${DIR}/simple_tpe/main.go
 go build -o ${BIN_DIR}/sobol ${DIR}/sobol/main.go
-go build -o ${BIN_DIR}/gorgonia_iris ${DIR}/gorgonia_iris/main.go
+#go build -o ${BIN_DIR}/concurrency ${DIR}/concurrency/main.go
+#go build -o ${BIN_DIR}/gorgonia_iris ${DIR}/gorgonia_iris/main.go
